ca74552a80d3a9c99dfecd381c98ba71eca2f877
Magno Guimarães <magno@fuerzastudio.com.br>
2022-01-27 15:39:37 -0300

    configurations for deploy on vercel

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const data = require('./data');
 const cors = require('cors');
 require('dotenv').config();
-const port = process.env.PORT || 5001; 
+const port = process.env.PORT || 5000; 
 
 const app = express();
 app.use(cors())
