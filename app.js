5398ac2fe6741bed44b76b10eac9384d6e5dcf00
Magno Guimarães <magno@fuerzastudio.com.br>
2022-01-27 14:37:08 -0300

    qurable api version 1

---
+++ app.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const data = require('./data');
+const cors = require('cors');
+  
+const app = express();
+app.use(cors())
+
+//Get items by query
+app.get('/api/items', (req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  const filters = req.query.q;
+ 
+  const filteredItems = data.filter(item => {
+     const searchValuesInData = 
+        item.name.toLowerCase().includes(filters.toLowerCase()) || 
+        item.id.toLowerCase().includes(filters.toLowerCase()) ||
+        item.description.toLowerCase().includes(filters.toLowerCase()) 
+    return searchValuesInData
+    
+  });
+  res.send({items: filteredItems});
+});
+
+
+//Get items by id
+app.get('/api/items/:id', (req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    const filters = req.params;
+    const filteredItems = data.filter(item => {
+       
+      let isValid = true;
+      for (key in filters) {
+        isValid = isValid && item[key] == filters[key];
+      }
+      return isValid;
+    });
+    res.send({items: filteredItems});
+  });
+  
+
+app.listen(5000, () => {
+  console.log('Server started!');
+});
+
+//Get items by creator
+app.get('/api/creators/:creator_id', (req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    const filters = req.params;
+    const filteredItems = data.filter(item => {
+       
+      let isValid = true;
+      for (key in filters) {
+        isValid = isValid && item.creator[key] == filters[key];
+      }
+      return isValid;
+    });
+    res.send({items: filteredItems});
+  });
+  
---
+++ data.js
@@ -0,0 +1,244 @@
+const data = [
+      {
+        id: "3245",
+        name: "Juliette Guerrero",
+        description: "Proident in nulla duis irure Lorem aliquip laborum voluptate esse irure culpa adipisicing pariatur nisi. Nostrud mollit id elit in consectetur commodo minim cillum nulla officia do. Minim tempor consectetur sit aliquip do eiusmod Lorem velit tempor ex sint. Reprehenderit consequat ullamco deserunt cupidatat velit consequat qui adipisicing sit magna Lorem exercitation. Elit ut ut velit mollit ipsum dolore id enim esse dolor labore ullamco eu. Amet nostrud consectetur do minim in nulla sint et officia dolore enim deserunt velit. Aliquip adipisicing culpa reprehenderit pariatur qui irure dolor fugiat labore proident est ipsum cillum.\r\n",
+        price: 1234.56,
+        image: "http://localhost:3000/assets/product-image.jpg",
+        creator: {
+          creator_id: "101",
+          creator_name: "El Burgues",
+          creator_image: "http://localhost:3000/assets/creator-3.png"
+        }
+      },
+      {
+        id: "3246",
+        name: "Jaime Reed",
+        description: "Minim duis commodo enim velit. Eiusmod consectetur id aute duis excepteur voluptate tempor dolor commodo adipisicing ullamco nulla. Anim ea sit laborum velit proident laboris ex reprehenderit sint aliquip. Incididunt ea do nulla id officia commodo ea dolore. Deserunt commodo proident adipisicing exercitation non eiusmod dolore aute. Anim nisi incididunt dolore laboris nostrud eiusmod sint consequat ullamco.\r\n",
+        price: 1234.56,
+        image: "http://localhost:3000/assets/product-image.jpg",
+        creator: {
+          creator_id: "101",
+          creator_name: "El Burgues",
+          creator_image: "http://localhost:3000/assets/creator-3.png"
+        }
+      },
+      {
+        id: "6588",
+        name: "Woodard Suarez",
+        description: "Eiusmod labore eiusmod cupidatat mollit voluptate sint nostrud enim ex mollit. Laboris fugiat eu ea quis cillum voluptate ullamco cupidatat pariatur. Do incididunt ad consequat excepteur irure. Adipisicing labore ad ipsum ad veniam nostrud irure consectetur est. Id nulla ea non nostrud reprehenderit est aliquip. Elit cupidatat anim minim ut aliqua do commodo ad id labore consequat.\r\n",
+        price: 1234.56,
+        image: "http://localhost:3000/assets/product-image.jpg",
+        creator: {
+          creator_id: "101",
+          creator_name: "El Burgues",
+          creator_image: "http://localhost:3000/assets/creator-3.png"
+        }
+      },
+      {
+        id: "9364",
+        name: "Bender Chambers",
+        description: "Fugiat ea dolor voluptate consequat commodo commodo. Sunt do adipisicing pariatur velit ullamco officia ipsum. Cupidatat tempor consectetur sit voluptate voluptate pariatur proident adipisicing. Officia sunt Lorem ipsum velit pariatur in laborum eiusmod. Sit ipsum amet deserunt id aliqua aliquip fugiat laboris sit exercitation id ipsum laboris. Elit qui aliquip enim mollit consequat esse aliquip do.\r\n",
+        price: 1234.56,
+        image: "http://localhost:3000/assets/product-image.jpg",
+        creator: {
+          creator_id: 102,
+          creator_name: "Creator 2",
+          creator_image: "http://localhost:3000/assets/creator-2.png"
+        }
+      },
+      {
+        id: "3547",
+        name: "Lisa Santos",
+        description: "Et elit amet aute irure culpa in ipsum esse occaecat ea anim deserunt Lorem voluptate. Culpa ut minim culpa in sunt non. Aliqua minim ipsum sit anim ipsum ipsum. Excepteur nulla qui tempor nisi nulla esse est consectetur laborum veniam irure dolor duis adipisicing.\r\n",
+        price: 1234.56,
+        image: "http://localhost:3000/assets/product-image.jpg",
+        creator: {
+          creator_id: 102,
+          creator_name: "Creator 2",
+          creator_image: "http://localhost:3000/assets/creator-2.png"
+        }
+      },
+      {
+        id: "9654",
+        name: "Clare Branch",
+        description: "In ex culpa quis pariatur adipisicing. Nulla quis fugiat quis eiusmod esse. Do eiusmod velit esse nisi ad. Velit aute nulla est eiusmod irure aute deserunt ea ex.\r\n",
+        price: 1234.56,
+        image: "http://localhost:3000/assets/product-image.jpg",
+        creator: {
+          creator_id: 102,
+          creator_name: "Creator 2",
+          creator_image: "http://localhost:3000/assets/creator-2.png"
+        }
+      },
+      {
+        id: "6548",
+        name: "Nelson Craft",
+        description: "Veniam deserunt duis amet sunt ipsum. Ea aliquip duis deserunt Lorem labore aliqua ipsum irure sint quis irure magna exercitation. Lorem laboris amet ipsum ipsum sit veniam quis excepteur. Elit eiusmod non anim laboris ad in adipisicing dolore. Amet adipisicing minim elit fugiat dolor incididunt Lorem quis enim dolore quis nisi. Dolor nisi elit magna labore et.\r\n",
+        price: 1234.56,
+        image: "http://localhost:3000/assets/product-image.jpg",
+        creator: {
+          creator_id: 103,
+          creator_name: "Creator 1",
+          creator_image: "http://localhost:3000/assets/creator-1.png"
+        }
+      },
+      {
+        id: "3457",
+        name: "Pearl Daugherty",
+        description: "Dolore esse pariatur in tempor occaecat ex elit qui occaecat pariatur incididunt eiusmod ad amet. Lorem duis cupidatat sunt laborum consequat fugiat occaecat. Enim aliqua do pariatur dolore aliquip Lorem fugiat. Magna ullamco quis exercitation esse dolor cupidatat tempor labore ipsum incididunt. Magna laborum ea sit deserunt qui dolor fugiat fugiat Lorem et nostrud id.\r\n",
+        price: 1234.56,
+        image: "http://localhost:3000/assets/product-image.jpg",
+        creator: {
+          creator_id: "101",
+          creator_name: "El Burgues",
+          creator_image: "http://localhost:3000/assets/creator-3.png"
+        }
+      },
+      {
+        id: "8593",
+        name: "Tracie Johns",
+        description: "Nulla deserunt id laboris pariatur occaecat Lorem aute culpa nulla sint. Eiusmod aliquip fugiat duis labore culpa. Irure laboris aliqua amet enim quis. Amet esse elit non eu incididunt elit et velit et sint non aliqua enim. Pariatur sint duis culpa elit quis aliqua ex voluptate id. Aute nisi magna et laborum duis anim.\r\n",
+        price: 1234.56,
+        image: "http://localhost:3000/assets/product-image.jpg",
+        creator: {
+          creator_id: "101",
+          creator_name: "El Burgues",
+          creator_image: "http://localhost:3000/assets/creator-3.png"
+        }
+      },
+      {
+        id: "7452",
+        name: "Shawna Lott",
+        description: "Incididunt incididunt deserunt elit nulla cupidatat. Laboris nulla dolore duis sunt adipisicing sunt eu pariatur id exercitation magna velit incididunt. Est fugiat irure do enim aliqua adipisicing magna anim id. Consectetur voluptate adipisicing et nostrud dolore cupidatat labore pariatur culpa nostrud. Commodo dolor excepteur aute incididunt adipisicing pariatur eu deserunt veniam est quis adipisicing.\r\n",
+        price: 1234.56,
+        image: "http://localhost:3000/assets/product-image.jpg",
+        creator: {
+          creator_id: "101",
+          creator_name: "El Burgues",
+          creator_image: "http://localhost:3000/assets/creator-3.png"
+        }
+      },
+      {
+        id: "9237",
+        name: "Wira Thecla",
+        description: "Incididunt incididunt deserunt elit nulla cupidatat. Laboris nulla dolore duis sunt adipisicing sunt eu pariatur id exercitation magna velit incididunt. Est fugiat irure do enim aliqua adipisicing magna anim id. Consectetur voluptate adipisicing et nostrud dolore cupidatat labore pariatur culpa nostrud. Commodo dolor excepteur aute incididunt adipisicing pariatur eu deserunt veniam est quis adipisicing.\r\n",
+        price: 1234.56,
+        image: "http://localhost:3000/assets/product-image.jpg",
+        creator: {
+          creator_id: "103",
+          creator_name: "Creator 1",
+          creator_image: "http://localhost:3000/assets/creator-1.png"
+        }
+      },
+      {
+        id: "23490",
+        name: "Boban Naira",
+        description: "Incididunt incididunt deserunt elit nulla cupidatat. Laboris nulla dolore duis sunt adipisicing sunt eu pariatur id exercitation magna velit incididunt. Est fugiat irure do enim aliqua adipisicing magna anim id. Consectetur voluptate adipisicing et nostrud dolore cupidatat labore pariatur culpa nostrud. Commodo dolor excepteur aute incididunt adipisicing pariatur eu deserunt veniam est quis adipisicing.\r\n",
+        price: 1234.56,
+        image: "http://localhost:3000/assets/product-image.jpg",
+        creator: {
+          creator_id: "101",
+          creator_name: "El Burgues",
+          creator_image: "http://localhost:3000/assets/creator-3.png"
+        }
+      },
+      {
+        id: "9734",
+        name: "Tomo Janne",
+        description: "Incididunt incididunt deserunt elit nulla cupidatat. Laboris nulla dolore duis sunt adipisicing sunt eu pariatur id exercitation magna velit incididunt. Est fugiat irure do enim aliqua adipisicing magna anim id. Consectetur voluptate adipisicing et nostrud dolore cupidatat labore pariatur culpa nostrud. Commodo dolor excepteur aute incididunt adipisicing pariatur eu deserunt veniam est quis adipisicing.\r\n",
+        price: 1234.56,
+        image: "http://localhost:3000/assets/product-image.jpg",
+        creator: {
+          creator_id: "103",
+          creator_name: "Creator 1",
+          creator_image: "http://localhost:3000/assets/creator-1.png"
+        }
+      },
+      {
+        id: "1234",
+        name: "Oddr Severina",
+        description: "Incididunt incididunt deserunt elit nulla cupidatat. Laboris nulla dolore duis sunt adipisicing sunt eu pariatur id exercitation magna velit incididunt. Est fugiat irure do enim aliqua adipisicing magna anim id. Consectetur voluptate adipisicing et nostrud dolore cupidatat labore pariatur culpa nostrud. Commodo dolor excepteur aute incididunt adipisicing pariatur eu deserunt veniam est quis adipisicing.\r\n",
+        price: 1234.56,
+        image: "http://localhost:3000/assets/product-image.jpg",
+        creator: {
+          creator_id: "102",
+          creator_name: "Creator 2",
+          creator_image: "http://localhost:3000/assets/creator-2.png"
+        }
+      },
+      {
+        id: "7432",
+        name: "Reinhard Eliot",
+        description: "Incididunt incididunt deserunt elit nulla cupidatat. Laboris nulla dolore duis sunt adipisicing sunt eu pariatur id exercitation magna velit incididunt. Est fugiat irure do enim aliqua adipisicing magna anim id. Consectetur voluptate adipisicing et nostrud dolore cupidatat labore pariatur culpa nostrud. Commodo dolor excepteur aute incididunt adipisicing pariatur eu deserunt veniam est quis adipisicing.\r\n",
+        price: 1234.56,
+        image: "http://localhost:3000/assets/product-image.jpg",
+        creator: {
+          creator_id: "103",
+          creator_name: "Creator 1",
+          creator_image: "http://localhost:3000/assets/creator-1.png"
+        }
+      },
+      {
+        id: "4568",
+        name: "Simon Jagdish",
+        description: "Incididunt incididunt deserunt elit nulla cupidatat. Laboris nulla dolore duis sunt adipisicing sunt eu pariatur id exercitation magna velit incididunt. Est fugiat irure do enim aliqua adipisicing magna anim id. Consectetur voluptate adipisicing et nostrud dolore cupidatat labore pariatur culpa nostrud. Commodo dolor excepteur aute incididunt adipisicing pariatur eu deserunt veniam est quis adipisicing.\r\n",
+        price: 1234.56,
+        image: "http://localhost:3000/assets/product-image.jpg",
+        creator: {
+          creator_id: "102",
+          creator_name: "Creator 2",
+          creator_image: "http://localhost:3000/assets/creator-2.png"
+        }
+      },
+      {
+        id: "3451",
+        name: "Zodwa Chijindum",
+        description: "Incididunt incididunt deserunt elit nulla cupidatat. Laboris nulla dolore duis sunt adipisicing sunt eu pariatur id exercitation magna velit incididunt. Est fugiat irure do enim aliqua adipisicing magna anim id. Consectetur voluptate adipisicing et nostrud dolore cupidatat labore pariatur culpa nostrud. Commodo dolor excepteur aute incididunt adipisicing pariatur eu deserunt veniam est quis adipisicing.\r\n",
+        price: 1234.56,
+        image: "http://localhost:3000/assets/product-image.jpg",
+        creator: {
+          creator_id: "103",
+          creator_name: "Creator 1",
+          creator_image: "http://localhost:3000/assets/creator-1.png"
+        }
+      },
+      {
+        id: "24361",
+        name: "Margaux Anastasia",
+        description: "Incididunt incididunt deserunt elit nulla cupidatat. Laboris nulla dolore duis sunt adipisicing sunt eu pariatur id exercitation magna velit incididunt. Est fugiat irure do enim aliqua adipisicing magna anim id. Consectetur voluptate adipisicing et nostrud dolore cupidatat labore pariatur culpa nostrud. Commodo dolor excepteur aute incididunt adipisicing pariatur eu deserunt veniam est quis adipisicing.\r\n",
+        price: 1234.56,
+        image: "http://localhost:3000/assets/product-image.jpg",
+        creator: {
+          creator_id: "103",
+          creator_name: "Creator 1",
+          creator_image: "http://localhost:3000/assets/creator-1.png"
+        }
+      },
+      {
+        id: "0456",
+        name: "Piripi Taliesin",
+        description: "Incididunt incididunt deserunt elit nulla cupidatat. Laboris nulla dolore duis sunt adipisicing sunt eu pariatur id exercitation magna velit incididunt. Est fugiat irure do enim aliqua adipisicing magna anim id. Consectetur voluptate adipisicing et nostrud dolore cupidatat labore pariatur culpa nostrud. Commodo dolor excepteur aute incididunt adipisicing pariatur eu deserunt veniam est quis adipisicing.\r\n",
+        price: 1234.56,
+        image: "http://localhost:3000/assets/product-image.jpg",
+        creator: {
+          creator_id: "102",
+          creator_name: "Creator 2",
+          creator_image: "http://localhost:3000/assets/creator-2.png"
+        }
+      },
+      {
+        id: "3465",
+        name: "Gisa Yngve",
+        description: "Incididunt incididunt deserunt elit nulla cupidatat. Laboris nulla dolore duis sunt adipisicing sunt eu pariatur id exercitation magna velit incididunt. Est fugiat irure do enim aliqua adipisicing magna anim id. Consectetur voluptate adipisicing et nostrud dolore cupidatat labore pariatur culpa nostrud. Commodo dolor excepteur aute incididunt adipisicing pariatur eu deserunt veniam est quis adipisicing.\r\n",
+        price: 1234.56,
+        image: "http://localhost:3000/assets/product-image.jpg",
+        creator: {
+          creator_id: "102",
+          creator_name: "Creator 2",
+          creator_image: "http://localhost:3000/assets/creator-2.png"
+        }
+      },
+];
+  
+module.exports = data;
